4ad11ae4ad6f67a5b699de87ad6b42b84a7575c5
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2022-06-15 11:03:31 -0500

    Add files via upload

---
+++ Others/find_3files.b.sh
@@ -0,0 +1,10 @@
+cp  ../../../4b_keptRegions.gene/merged.bed    ./merged.bed    
+cp  ../../../0_BAM/Phenotype_Genotype/BA9.genotype.txt   ./BA9.genotype.txt
+
+sleep 3m
+
+perl   find_3files.pl   -file1  All.txt      -file2   merged.bed      -file3 BA9.genotype.txt
+
+
+
+
