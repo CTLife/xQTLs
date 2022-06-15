5b8724a5f51fc594c76429a5de8772dcf547c603
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2022-06-15 11:11:11 -0500

    Update find_3files.b.sh

--- Others/find_3files.b.sh
+++ Others/find_3files.b.sh
@@ -4,7 +4,3 @@ cp  ../../../0_BAM/Phenotype_Genotype/BA9.genotype.txt   ./BA9.genotype.txt
 sleep 3m
 
 perl   find_3files.pl   -file1  All.txt      -file2   merged.bed      -file3 BA9.genotype.txt
-
-
-
-
