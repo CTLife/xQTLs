8b21e5f1254a95a373fdf54f152764bf0a9289cc
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2022-09-05 09:48:00 -0500

    Delete 5_QTLtools_pca.SNP.sh

--- 5_QTLtools_pca.SNP.sh
+++
@@ -1,9 +0,0 @@
-output_g=5_PCA_genotype
-mkdir $output_g
-
-file1=SNPs
-QTLtools pca  --vcf  0_BAM/Phenotype_Genotype/BA9.recode.vcf.gz   --scale   --center  --maf 0.1 --distance 5000  --out   $output_g/$file1.vcf   --log $output_g/$file1.vcf.Log  >   $output_g/$file1.vcf.runLog.txt 2>&1                
-
-
-
-
