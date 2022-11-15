d57bde91a1857bec6a49cfd51b5e2d5969ef36c4
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2022-11-15 14:18:32 -0600

    Delete 6_mergedCounts.sh

--- 1_All/6_mergedCounts.sh
+++
@@ -1,8 +0,0 @@
-nohup time  perl 6_mergedCounts.pl   -in 5_TPM_RPKM/finalMergedPeaks/IP/forward       -out 6_mergedCounts/finalMergedPeaks/IP/forward      > 6_mergedCounts.1.runLog.txt  2>&1    &       
-nohup time  perl 6_mergedCounts.pl   -in 5_TPM_RPKM/finalMergedPeaks/Input/forward    -out 6_mergedCounts/finalMergedPeaks/Input/forward   > 6_mergedCounts.2.runLog.txt  2>&1    &       
-
-nohup time  perl 6_mergedCounts.pl   -in 5_TPM_RPKM/hg38.gencode.v39.chr1-22/Input/forward           -out 6_mergedCounts/hg38.gencode.v39.chr1-22/Input/forward             > 6_mergedCounts.3.runLog.txt  2>&1    &       
-
-nohup time  perl 6_mergedCounts.pl   -in 5_TPM_RPKM/hg38.gencode.v39.chr1-22.Collapsed/Input/forward    -out 6_mergedCounts/hg38.gencode.v39.chr1-22.Collapsed/Input/forward   > 6_mergedCounts.4.runLog.txt  2>&1    &       
-
-
